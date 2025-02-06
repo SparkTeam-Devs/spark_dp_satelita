ebd6f730a02633c3719806d54a2ff198338a9875
Igor Matnyak <igormatnak092@gmail.com>
2025-02-06 14:53:43 -0800

    feat: fix git workflows

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export",
-	basePath: "/",
-	assetPrefix: "/",
+
+	images: {
+		unoptimized: true,
+	},
 
   	sassOptions: {
 		additionalData: `@import "src/styles/_mixins.scss"; @import "src/styles/_fonts.scss";  @import "src/styles/_media.scss";`,
 	},
 	webpack(config) {
+
 		
 		config.module.rules.push({
 			test: /\.svg$/,
 			use: ["@svgr/webpack"],
+
 		});
 
 		return config
